54c52f844c8fac1948f94dd7377c273ef4491618
Andrei SILIN <andrei.silin@studi.fr>
2024-08-05 02:15:29 +0200

    debug env

--- src/api/subs/subs.ts
+++ src/api/subs/subs.ts
@@ -3,6 +3,7 @@ import axios, {AxiosResponse} from "axios";
 const URL = import.meta.env.VITE_REACT_API_URL
 export const getUserDataFromBot = async (userId: number): Promise<Subscriber> => {
     console.log('API URL ------> ', URL)
+    console.log('ENV VARS ------>', import.meta.env)
     try {
         const response: AxiosResponse<Subscriber> = await axios.post(URL + "/api/subs/one", {user_id: userId})
         return response.data
