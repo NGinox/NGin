db256874dd79953733a369b52677123567634561
Andrei SILIN <andrei.silin@studi.fr>
2024-08-27 14:52:54 +0200

    change energy to increase

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,4 @@ export const MAX_ENERGY = 50
 export const ENERGY_TO_REDUCE = 0.1
 export const DELAY_OF_TOKENS_SYNC = 1000
 export const DELAY_OF_INCREASING_OF_ENERGY = 1000
-export const ENERGY_TO_INCREASE = 0.1
+export const ENERGY_TO_INCREASE = 1
