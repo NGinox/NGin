e219f927cb1d7f7cc299cd433690b01def6029c7
Andrei SILIN <andrei.silin@studi.fr>
2024-09-03 16:09:26 +0200

    fix ui

--- src/components/pages/tasks/Tasks.tsx
+++ src/components/pages/tasks/Tasks.tsx
@@ -18,9 +18,12 @@ const Tasks = () => {
             <div className="text-xs text-center font-futuraRegular">
                 <span className="text-sm">Here you can earn more tokens by completing the tasks!</span>
             </div>
-            <div className="grid grid-cols-1 gap-4 mt-6 pb-4">
-
+            <div className="mt-6">
                 <TaskReferral/>
+            </div>
+
+            <div className="grid grid-cols-1 gap-2 mt-6 pb-4">
+
 
                 {
                     subscriber.tasks?.map(task => <TaskItem task={task}/>)
